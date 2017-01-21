4b533b63763012d85dba326503908b74d13afc6d
marioluan <mariosouzaluan@gmail.com>
2017-01-21 20:35:12 -0200

    fixed errors from eslint's rules

--- server/models/job.js
+++ server/models/job.js
@@ -2,33 +2,31 @@
 
 var app = require('../server.js');
 
-module.exports = function (Job) {
+module.exports = function(Job) {
+  // TODO: move to configuration and load from there.
+  var pagination = {
+    default: 10,
+    max: 20,
+  };
 
-    // TODO: move to configuration and load from there.
-    var pagination = {
-        default: 10,
-        max: 20
-    };
+  /**
+   * Job.beforeRemote intercepts find operations and apply a default
+   * page size, in case the user attempts to load so much data.
+   *
+   * See:
+   * https://strongloop.com/strongblog/working-with-pagination-and-loopback/.
+   */
+  Job.beforeRemote('find', function defaultPagination(ctx, instance, next) {
+    if (!ctx.args.filter)
+      ctx.args.filter = {};
 
-    /**
-     * Job.beforeRemote intercepts find operations and apply a default
-     * page size, in case the user attempts to load so much data.
-     *
-     * See:
-     * https://strongloop.com/strongblog/working-with-pagination-and-loopback/.
-     */
-    Job.beforeRemote('find', function applyDefaultPagination(ctx, instance, next) {
+    // set default page size
+    if (!ctx.args.filter.limit)
+      ctx.args.filter.limit = pagination.default;
+    // load at most pagination.max items
+    else if (ctx.args.filter.limit > pagination.max)
+      ctx.args.filter.limit = pagination.max;
 
-        if (!ctx.args.filter)
-            ctx.args.filter = {};
-
-        // set default page size
-        if (!ctx.args.filter.limit)
-            ctx.args.filter.limit = pagination.default;
-        // load at most pagination.max items
-        else if (ctx.args.filter.limit > pagination.max)
-            ctx.args.filter.limit = pagination.max;
-
-        next();
-    });
+    next();
+  });
 };
